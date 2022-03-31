ece695bf14ed6159ad571d35d9fb3d9d9f343f1b
Cleidison Silva <cleidison.dev@gmail.com>
2022-03-31 12:30:48 -0300

    Pegando todos os dados da API

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 const express = require("express");
 const app = express();
+const data = require("../data.json");
 
 const PORT = 3000;
+
+app.get('/', (req, res) => {
+  res.json(data);
+})
 
 app.listen(PORT, () => console.log(`Server is running on ${PORT}`));
